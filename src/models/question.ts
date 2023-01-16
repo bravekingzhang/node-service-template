9cdc32872b9a01e6ba69218b87233b4935494dc8
hoollyzhang <hoollyzhang@tencent.com>
2023-01-16 18:15:27 +0800

    feat: version版本

--- src/models/question.ts
+++ src/models/question.ts
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 // 数据模型
 const QuestionSchema = new Schema({
-  question: { type: String, required: true, unique: true },
-  answer: { type: String, required: true },
+  version: { type: Number, required: true, unique: true },
+  questions: { type: String, required: true },
 });
 export const Question = model("Question", QuestionSchema);
